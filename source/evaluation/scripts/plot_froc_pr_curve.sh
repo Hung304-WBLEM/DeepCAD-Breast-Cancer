9aa8acb4b95d1b17c667a6cec49085adf64d7f90
Hung304-WBLEM <hungvq.blem.304@gmail.com>
2021-10-07 10:24:52 -0500

    update

---
+++ source/evaluation/scripts/plot_froc_pr_curve.sh
@@ -0,0 +1,8 @@
+cd ..
+python plot_eval_curve.py -gt /home/hqvo2/Projects/Breast_Cancer/data/processed_data2/mass/test/annotation_coco_with_classes.json \
+                        -p /home/hqvo2/Projects/Breast_Cancer/experiments/cbis_ddsm_detection/mass/frcnn_cad_ddsm_mode_8000_12800/frcnn_cad_test_ddsm_mode_8000_12800.json \
+                        -bb all \
+                        -s /home/hqvo2/Projects/Breast_Cancer/experiments/cbis_ddsm_detection/mass/frcnn_cad_ddsm_mode_8000_12800
+
+
+
